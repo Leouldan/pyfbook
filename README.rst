787aa258edf11dca6f398e2e9991db725caa5f98
Vincent Monet <vincent@dacker.co>
2018-04-03 16:20:20 +0200

    First commit

---
+++ README.rst
@@ -0,0 +1,21 @@
+pyfbook
+=====
+
+A python package to easily collect data from Facebook APIs (Marketing, Page and Graph)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+1) Installation
+'''''''''''''''
+
+Open a terminal and install pyfbook package
+
+::
+
+    pip install pyfbook
+
+
+2) Use
+''''''
+
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyfbook',
+    version='0.0.0',
+    description='Easily collect data from Facebook APIs',
+    long_description=readme,
+    author='Dacker',
+    author_email='hello@dacker.co',
+    url='https://github.com/dacker-team/pyfbook',
+    license=license,
+    keywords='collect data facebook api',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3',
+
+)

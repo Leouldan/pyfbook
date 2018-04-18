4c4537a663983caa36b8b6efd134ea63c4438ad0
Vincent Monet <vincent@dacker.co>
2018-04-18 12:04:48 +0200

    change version 0.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyfbook',
-    version='0.0.0',
+    version='0.0.1',
     description='Easily collect data from Facebook APIs',
     long_description=readme,
     author='Dacker',

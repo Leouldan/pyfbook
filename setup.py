fd49574be92ae2279790d136b6eb1d42bd947f46
Vincent Monet <vincent@dacker.co>
2018-10-21 19:08:59 +0200

    setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyfbook',
-    version='0.0.18',
+    version='0.0.19',
     description='Easily collect data from Facebook APIs',
     long_description=readme,
     author='Dacker',

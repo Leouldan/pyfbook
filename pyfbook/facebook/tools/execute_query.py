24d8221e335acd1e58928c1acdb5684c642582e6
Vincent Monet <vincent@dacker.co>
2019-08-29 19:17:56 +0200

    update

--- pyfbook/facebook/tools/execute_query.py
+++ pyfbook/facebook/tools/execute_query.py
@@ -1,12 +1,16 @@
+import pyred
 import pyzure
-from pyfbook.facebook.config import get_config
 
 
 def execute_query(query, config):
     if config.get('instance').get('type') == 'azure':
         return pyzure.execute_query(config.get('instance').get('name'), query)
+    if config.get('instance').get('type') == 'redshift':
+        return pyred.execute_query(config.get('instance').get('name'), query)
 
 
 def send_data(data, config, replace=False):
     if config.get('instance').get('type') == 'azure':
         return pyzure.send_data(config.get('instance').get('name'), data, replace=replace)
+    if config.get('instance').get('type') == 'redshift':
+        return pyred.send_to_redshift(config.get('instance').get('name'), data, replace=replace)
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyfbook',
-    version='0.0.25',
+    version='0.0.26',
     description='Easily collect data from Facebook APIs',
     long_description=readme,
     author='Dacker',

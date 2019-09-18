82042d2496138dce42612aa78dceabeb3a510207
Vincent Monet <vincent@dacker.co>
2019-09-18 17:13:29 +0200

    date null

--- pyfbook/facebook/modules/report/date.py
+++ pyfbook/facebook/modules/report/date.py
@@ -22,7 +22,10 @@ def define_start_date(config, report, time_increment, account, async):
                 WHERE account_id='%s'
                 """ % (table_name, account["account_id"])
     start_date = execute_query(query, config)[0]["start_date"]
-    return str(datetime.datetime.strptime(str(start_date)[:10], '%Y-%m-%d') - datetime.timedelta(days=28))[:10]
+    if start_date:
+        return str(datetime.datetime.strptime(str(start_date)[:10], '%Y-%m-%d') - datetime.timedelta(days=28))[:10]
+    else:
+        return None
 
 
 # noinspection SqlNoDataSourceInspection
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyfbook',
-    version='0.0.40',
+    version='0.0.41',
     description='Easily collect data from Facebook APIs',
     long_description=readme,
     author='Dacker',
